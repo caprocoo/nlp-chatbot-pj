7e8ca3850a2e8a99dbfff4a19d78381b78afa812
caprocoo <91231618+caprocoo@users.noreply.github.com>
2024-06-05 13:22:46 +0900

    chapter 6 개체명 인식

--- chapter/chapter6/deep_learning.py
+++ chapter/chapter6/deep_learning.py
@@ -8,6 +8,7 @@
 #   5. RNN 모델
 #   6. LSTM 모델
 #   7. 양방향 LSTM 모델
+#   8. 개체명 인식 (NER)
 
 
 # 1. MNIST 분류모델 학습
@@ -340,139 +341,286 @@
 # plt.show()
 
 
-# 6. LSTM 모델
-import numpy as np
+# # 6. LSTM 모델
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Flatten, Dense, LSTM
+#
+#
+# def split_sequence(sequence, step):
+#     x, y = list(), list()
+#
+#     for i in range(len(sequence)):
+#         end_idx = i + step
+#         if end_idx > len(sequence) - 1:
+#             break
+#
+#         seq_x, seq_y = sequence[i:end_idx], sequence[end_idx]
+#         x.append(seq_x)
+#         y.append(seq_y)
+#
+#     return np.array(x), np.array(y)
+#
+#
+# # sin 함수 학습 데이터
+# x = [i for i in np.arange(start=-10, stop=10, step=0.1)]
+# train_y = [np.sin(i) for i in x]
+#
+# # 하이퍼 파라미터
+# n_timesteps = 15
+# n_features = 1
+#
+# # 시퀀스 나누기
+# # train_x.shape => (samples, timesteps)
+# # train_y.shape => (samples)
+# train_x, train_y = split_sequence(train_y, step=n_timesteps)
+# print('shape x:{} / y:{}'.format(train_x.shape, train_y.shape))
+#
+# # LSTM 입력 벡터 크기를 맞추기 위해 벡터 차원 크기 변경
+# # reshape from [samples, timesteps] into [samples, timesteps, features]
+# train_x = train_x.reshape(train_x.shape[0], train_x.shape[1], n_features)
+# print('train_x.shape = {}'.format(train_x.shape))
+# print('train_y.shape = {}'.format(train_y.shape))
+#
+# # LSTM 모델 정의
+# model = Sequential()
+# model.add(LSTM(units=10, return_sequences=False, input_shape=(n_timesteps, n_features)))
+# model.add(Dense(1))
+# model.compile(optimizer='adam', loss='mse')
+#
+# # 모델 학습
+# np.random.seed(0)
+# from tensorflow.keras.callbacks import EarlyStopping
+#
+# early_stopping = EarlyStopping(
+#     monitor='loss',
+#     patience=5,
+#     mode='auto'
+# )
+# history = model.fit(train_x, train_y, epochs=1000, callbacks=[early_stopping])
+#
+# # loss 그래프 생성
+# plt.plot(history.history['loss'], label='loss')
+# plt.legend(loc='upper right')
+# plt.show()
+#
+# # 테스트 데이터셋 생성
+# test_x = np.arange(10, 20, 0.1)
+# calc_y = np.cos(test_x)
+#
+# # RNN 모델 예측 및 로그 저장
+# test_y = calc_y[:n_timesteps]
+# for i in range(len(test_x) - n_timesteps):
+#     net_input = test_y[i: i + n_timesteps]
+#     net_input = net_input.reshape((1, n_timesteps, n_features))
+#     train_y = model.predict(net_input, verbose=0)
+#     print(test_y.shape, train_y.shape, i, i + n_timesteps)
+#     test_y = np.append(test_y, train_y)
+#
+# # 예측 결과 그래프 그리기
+# plt.plot(test_x, calc_y, label='ground truth', color='orange')
+# plt.plot(test_x, test_y, label='predictions', color='blue')
+#
+# plt.legend(loc='upper left')
+# plt.ylim(-2, 2)
+# plt.show()
+#
+# # 7. 양방향 LSTM 모델
+# import numpy as np
+# from random import random
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Bidirectional, Dense, LSTM, TimeDistributed
+#
+#
+# # 시퀀스 생성
+# def get_sequence(n_timesteps):
+#     # 0~1 사이의 랜덤 시퀀스 생성
+#     X = np.array([random() for _ in range(n_timesteps)])
+#
+#     # 클래스 분류 기준
+#     limit = n_timesteps / 4.0
+#
+#     # 누적합 시퀀스에서 클래스 결정
+#     # 누적합 항목이 limit 보다 작은 경우 0, 아닌 경우 1로 분류
+#     y = np.array([0 if x < limit else 1 for x in np.cumsum(X)])
+#
+#     # LSTM 입력을 위해 3차원 텐서 형태로 변경
+#     X = X.reshape(1, n_timesteps, 1)
+#     y = y.reshape(1, n_timesteps, 1)
+#     return X, y
+#
+# # 하이퍼파라미터 정의
+# n_units = 20
+# n_timesteps = 4
+#
+# # 양방향 LSTM 모델 정의
+# model = Sequential()
+# model.add(Bidirectional(LSTM(n_units, return_sequences=True, input_shape=(n_timesteps,1))))
+# model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#
+# # 모델 학습
+# # 에포크마다 학습 데이터를 생성해서 학습
+# for epoch in range(1000):
+#     X, y = get_sequence(n_timesteps)
+#     model.fit(X, y, epochs=1, batch_size=1, verbose=2)
+#
+# # 모델 평가
+# X, y = get_sequence(n_timesteps)
+# yhat = model.predict_classes(X, verbose=0)
+# for i in range(n_timesteps):
+#     print('실젯값 : ', y[0, i], '예측값 : ', yhat[0, i])
+
+
+# 8. 개체명 인식 (NER)
+# : 문장 내에 포함된 어떤 단어가 인물, 장소, 날짜 등을 의미하는 단어인지 인식하는 것
+
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Flatten, Dense, LSTM
-
-
-def split_sequence(sequence, step):
-    x, y = list(), list()
-
-    for i in range(len(sequence)):
-        end_idx = i + step
-        if end_idx > len(sequence) - 1:
-            break
-
-        seq_x, seq_y = sequence[i:end_idx], sequence[end_idx]
-        x.append(seq_x)
-        y.append(seq_y)
-
-    return np.array(x), np.array(y)
-
-
-# sin 함수 학습 데이터
-x = [i for i in np.arange(start=-10, stop=10, step=0.1)]
-train_y = [np.sin(i) for i in x]
-
-# 하이퍼 파라미터
-n_timesteps = 15
-n_features = 1
-
-# 시퀀스 나누기
-# train_x.shape => (samples, timesteps)
-# train_y.shape => (samples)
-train_x, train_y = split_sequence(train_y, step=n_timesteps)
-print('shape x:{} / y:{}'.format(train_x.shape, train_y.shape))
-
-# LSTM 입력 벡터 크기를 맞추기 위해 벡터 차원 크기 변경
-# reshape from [samples, timesteps] into [samples, timesteps, features]
-train_x = train_x.reshape(train_x.shape[0], train_x.shape[1], n_features)
-print('train_x.shape = {}'.format(train_x.shape))
-print('train_y.shape = {}'.format(train_y.shape))
-
-# LSTM 모델 정의
-model = Sequential()
-model.add(LSTM(units=10, return_sequences=False, input_shape=(n_timesteps, n_features)))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mse')
-
-# 모델 학습
-np.random.seed(0)
-from tensorflow.keras.callbacks import EarlyStopping
-
-early_stopping = EarlyStopping(
-    monitor='loss',
-    patience=5,
-    mode='auto'
-)
-history = model.fit(train_x, train_y, epochs=1000, callbacks=[early_stopping])
-
-# loss 그래프 생성
-plt.plot(history.history['loss'], label='loss')
-plt.legend(loc='upper right')
-plt.show()
-
-# 테스트 데이터셋 생성
-test_x = np.arange(10, 20, 0.1)
-calc_y = np.cos(test_x)
-
-# RNN 모델 예측 및 로그 저장
-test_y = calc_y[:n_timesteps]
-for i in range(len(test_x) - n_timesteps):
-    net_input = test_y[i: i + n_timesteps]
-    net_input = net_input.reshape((1, n_timesteps, n_features))
-    train_y = model.predict(net_input, verbose=0)
-    print(test_y.shape, train_y.shape, i, i + n_timesteps)
-    test_y = np.append(test_y, train_y)
-
-# 예측 결과 그래프 그리기
-plt.plot(test_x, calc_y, label='ground truth', color='orange')
-plt.plot(test_x, test_y, label='predictions', color='blue')
-
-plt.legend(loc='upper left')
-plt.ylim(-2, 2)
-plt.show()
-
-# 7. 양방향 LSTM 모델
+import tensorflow as tf
+from tensorflow.keras import preprocessing
+from sklearn.model_selection import train_test_split
 import numpy as np
-from random import random
+
+
+# 학습 파일 불러오기
+def read_file(file_name):
+    sents = []
+    with open(file_name, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for idx, l in enumerate(lines):
+            if l[0] == ';' and lines[idx + 1][0] == '$':
+                this_sent = []
+            elif l[0] == '$' and lines[idx - 1][0] == ';':
+                continue
+            elif l[0] == '\n':
+                sents.append(this_sent)
+            else:
+                this_sent.append(tuple(l.split()))
+    return sents
+
+
+# 학습용 말뭉치 데이터를 불러옴
+corpus = read_file('../../ner_train_origin.txt')
+
+# 말뭉치 데이터에서 단어와 BIO 태그만 불러와 학습용 데이터셋 생성
+sentences, tags = [], []
+for t in corpus:
+    tagged_sentence = []
+    sentence, bio_tag = [], []
+    for w in t:
+        tagged_sentence.append((w[1], w[3]))
+        sentence.append(w[1])
+        bio_tag.append(w[3])
+
+    sentences.append(sentence)
+    tags.append(bio_tag)
+
+print('샘플 크기 : \n', len(sentences))
+print('0번째 샘플 문장 시퀀스 : \n', sentences[0])
+print('0번째 샘플 bio 태그 : \n', tags[0])
+print('샘플 문장 시퀀스 최대 길이 : ', max(len(l) for l in sentences))
+print('샘플 문장 시퀀스 평균 길이 : ', (sum(map(len, sentences)) / len(sentences)))
+
+# 토크나이저 정의
+sent_tokenizer = preprocessing.text.Tokenizer(oov_token='OOV')  # 첫 번째 인덱스에는 OOV 사용
+sent_tokenizer.fit_on_texts(sentences)
+tag_tokenizer = preprocessing.text.Tokenizer(lower=False)  # 태그 정보는 lower=False 소문자로 변환하지 않는다
+tag_tokenizer.fit_on_texts(tags)
+
+# 단어 사전 및 태그 사전 크기
+vocab_size = len(sent_tokenizer.word_index) + 1
+tag_size = len(tag_tokenizer.word_index) + 1
+print('BIO 태그 사전 크기 : ', tag_size)
+print('단어 사전 크기 : ', vocab_size)
+
+# 학습용 단어 시퀀스 생성
+x_train = sent_tokenizer.texts_to_sequences(sentences)
+y_train = tag_tokenizer.texts_to_sequences(tags)
+print(x_train[0])
+print(y_train[0])
+
+# index to word / index to NER 정의
+index_to_word = sent_tokenizer.index_word
+index_to_ner = tag_tokenizer.index_word
+index_to_ner[0] = 'PAD'
+
+# 시퀀스 패딩 처리
+max_len = 40
+x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=max_len, padding='post')
+y_train = preprocessing.sequence.pad_sequences(y_train, maxlen=max_len, padding='post')
+
+# 학습 데이터와 테스트 데이터를 8:2 비율로 분리
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=2, random_state=0)
+
+# 출력 데이터를 원-핫 인코딩
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=tag_size)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=tag_size)
+print('학습 샘플 시퀀스 형상 : ', x_train.shape)
+print('학습 샘플 레이블 형상 : ', y_train.shape)
+print('테스트 샘플 시퀀스 형상 : ', x_test.shape)
+print('테스트 샘플 레이블 형상 : ', y_test.shape)
+
+# 모델 정의 (Bi-LSTM)
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Bidirectional, Dense, LSTM, TimeDistributed
+from tensorflow.keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from tensorflow.keras.optimizers import Adam
 
-
-# 시퀀스 생성
-def get_sequence(n_timesteps):
-    # 0~1 사이의 랜덤 시퀀스 생성
-    X = np.array([random() for _ in range(n_timesteps)])
-
-    # 클래스 분류 기준
-    limit = n_timesteps / 4.0
-
-    # 누적합 시퀀스에서 클래스 결정
-    # 누적합 항목이 limit 보다 작은 경우 0, 아닌 경우 1로 분류
-    y = np.array([0 if x < limit else 1 for x in np.cumsum(X)])
-
-    # LSTM 입력을 위해 3차원 텐서 형태로 변경
-    X = X.reshape(1, n_timesteps, 1)
-    y = y.reshape(1, n_timesteps, 1)
-    return X, y
-
-# 하이퍼파라미터 정의
-n_units = 20
-n_timesteps = 4
-
-# 양방향 LSTM 모델 정의
 model = Sequential()
-model.add(Bidirectional(LSTM(n_units, return_sequences=True, input_shape=(n_timesteps,1))))
-model.add(TimeDistributed(Dense(1, activation='sigmoid')))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(Embedding(input_dim=vocab_size, output_dim=30, mask_zero=True))
+model.add(Bidirectional(LSTM(200, return_sequences=True, dropout=0.50, recurrent_dropout=0.25, input_shape=(max_len, 30))))
+model.add(TimeDistributed(Dense(tag_size, activation='softmax')))
+model.compile(loss='categorical_crossentropy', optimizer=Adam(0.01), metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=128, epochs=10)
 
-# 모델 학습
-# 에포크마다 학습 데이터를 생성해서 학습
-for epoch in range(1000):
-    X, y = get_sequence(n_timesteps)
-    model.fit(X, y, epochs=1, batch_size=1, verbose=2)
-
-# 모델 평가
-X, y = get_sequence(n_timesteps)
-yhat = model.predict_classes(X, verbose=0)
-for i in range(n_timesteps):
-    print('실젯값 : ', y[0, i], '예측값 : ', yhat[0, i])
+print('평가 결과 : ', model.evaluate(x_test, y_test)[1])
 
 
+# 시퀀스를 NER 태그로 변환
+def sequences_to_tag(sequences):
+    result = []
+    for sequence in sequences:
+        temp = []
+        for pred in sequence:
+            pred_index = np.argmax(pred)
+            temp.append(index_to_ner[pred_index].replace('PAD', '0'))
+        result.append(temp)
+    return result
 
+
+# 테스트 데이터셋의 NER 예측
+y_predicted = model.predict(x_test)  # (711, 40)  => model => (711, 40, 8)
+pred_tags = sequences_to_tag(y_predicted)  # 예측된 NER
+test_tags = sequences_to_tag(y_test)  # 실제 NER
+
+# F1 스코어 계산을 위해 사용
+from seqeval.metrics import f1_score, classification_report
+
+print(classification_report(test_tags, pred_tags))
+print('F1-score: {:.1%}'.format(f1_score(test_tags, pred_tags)))
+
+# 새로운 유형의 문장 NER 예측
+word_to_index = sent_tokenizer.word_index
+new_sentence = '삼성전자 출시 스마트폰 오늘 애플 도전장 내밀다.'.split()
+new_x = []
+for w in new_sentence:
+    try:
+        new_x.append(word_to_index.get(w, 1))
+    except KeyError:
+        # 모르는 단어의 경우 OOV
+        new_x.append(word_to_index['OOV'])
+
+print('새로운 유형의 시퀀스 : ', new_x)
+new_padded_seqs = preprocessing.sequence.pad_sequences([new_x], padding='post', value=0, maxlen=max_len)
+
+# NER 예측
+p = model.predict(np.array([new_padded_seqs[0]]))
+p = np.argmax(p, axis=1)  # 예측된 NER 인덱스값 추출
+
+print('{:10} {:5}'.format('단어', '예측된 NER'))
+print('-' * 50)
+for w, pred in zip(new_sentence, p[0]):
+    print('{:10} {:5}'.format(w, index_to_ner[pred]))
 
 # Chapter .  (p. ~ p.)
 
